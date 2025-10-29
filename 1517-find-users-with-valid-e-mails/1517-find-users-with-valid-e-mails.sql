e168836b4f0ecbd86b7ca2adc91c5c5350cc3797
developanda <161421211+developanda@users.noreply.github.com>
2025-10-29 16:19:28 +0900

    Time: 920 ms (11.94%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Users as u
-WHERE (u.mail REGEXP '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com$','c');
+WHERE regexp_like(mail, '^[A-Za-z]+[A-Za-z0-9_.-]*@leetcode[.]com$','c')
