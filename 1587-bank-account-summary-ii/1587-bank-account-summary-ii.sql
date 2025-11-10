ed95fc7d024a4a9193ca462f7dd7cc4af26aee55
developanda <161421211+developanda@users.noreply.github.com>
2025-11-10 14:32:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH TMP AS (
+SELECT u.name as name, sum(amount) as balance
+FROM Transactions as t
+    LEFT JOIN Users as u ON t.account = u.account
+GROUP BY u.name
+)
+SELECT name, balance
+FROM TMP
+WHERE balance >= 10000
