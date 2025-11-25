0237fd0a5799e9d9ac59e305b243bd34e1fb808c
developanda <161421211+developanda@users.noreply.github.com>
2025-11-25 14:45:33 +0900

    Time: 292 ms (51.43%), Space: 67.3 MB (79.75%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.py
+++ 1683-invalid-tweets/1683-invalid-tweets.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
-    tweets['con_len'] = tweets['content'].str.len()
-    return pd.DataFrame(tweets[tweets['con_len']>15]['tweet_id'])
+    return pd.DataFrame(tweets[tweets['content'].str.len()>15]['tweet_id'])
