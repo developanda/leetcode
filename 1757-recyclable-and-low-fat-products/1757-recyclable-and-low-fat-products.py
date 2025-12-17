0a7892d07d3fd8711bb2bd219d5d48435147c15a
developanda <161421211+developanda@users.noreply.github.com>
2025-12-17 12:58:36 +0900

    Time: 282 ms (77.77%), Space: 67.3 MB (66.83%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(products[(products['low_fats']=='Y')&(products['recyclable']=='Y')]['product_id'])
