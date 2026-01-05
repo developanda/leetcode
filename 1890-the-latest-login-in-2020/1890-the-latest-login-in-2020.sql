f71711be220810641af8f04439159579f192b25e
developanda <161421211+developanda@users.noreply.github.com>
2026-01-05 14:34:25 +0900

    Time: 599 ms (53.01%), Space: 0B (100%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -3,7 +3,4 @@ SELECT user_id, max(time_stamp) as last_stamp
 FROM Logins
 WHERE  (time_stamp >= '2020-01-01 00:00:00') 
        AND (time_stamp < '2021-01-01 00:00:00') 
-       AND (user_id in (SELECT DISTINCT(user_id)
-                        FROM Logins
-                        WHERE (time_stamp >= '2020-01-01 00:00:00') AND (time_stamp < '2021-01-01 00:00:00')))
 GROUP BY user_id
