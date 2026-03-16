c198e3bd100e33b349d0d5d038bd0a1d94616630
developanda <161421211+developanda@users.noreply.github.com>
2026-03-16 16:37:27 +0900

    Time: 345 ms (5.99%), Space: 67.3 MB (6.59%) - LeetHub

---
+++ 3436-find-valid-emails/3436-find-valid-emails.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def find_valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    users['email_part1'] = users['email'].str.split('@').str[0]
+    users['email_part2'] = users['email'].str.split('@').str[1]
+    users['email_part3'] = users['email_part2'].str.split('.').str[1]
+    users['email_part2'] = users['email_part2'].str.split('.').str[0]
+
+    users['email_part1'].replace('_','')
+
+    print(users['email'].str.contains('@'))
+    print(users['email_part3'] == 'com')
+    print(users['email_part1'].str.isalnum())
+    print(users['email_part1'].str.isalpha())
+
+    return users[(users['email'].str.contains('@'))
+                  &(users['email_part3'] == 'com')
+                  &(users['email_part1'].str.isalnum())
+                  &(users['email_part2'].str.isalpha())].sort_values('user_id')[['user_id','email']]
